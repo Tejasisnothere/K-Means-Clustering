e3c201baec0f20f21a5bb1b896edd5d4e1b6da60
Tejasisnothere <tejaskadam209@gmail.com>
2025-04-01 21:12:40 +0530

    final code

--- kmeanscpp.cpp
+++ kmeanscpp.cpp
@@ -1,204 +1,62 @@
+
 #include <iostream>
-#include <cmath>
-#include <random>
 #include <vector>
-#include <fstream>
-#include <unordered_set>
-#include <chrono>
-
+#include <cmath>
+#include <limits>
 using namespace std;
-using namespace chrono;
-random_device rd;
-mt19937 gen(rd());
-
-class Point
-{
-public:
-    double X, Y;
-    Point *centroid;
-    Point(int ll = 0, int ul = 100)
-    {
-        uniform_real_distribution<double> dist(ll, ul);
-        X = dist(gen);
-        Y = dist(gen);
-    }
+struct Point {
+    float x, y;
+    int cluster;
 };
-
-double euc(Point p1, Point p2)
-{
-    return sqrt(pow(p1.X - p2.X, 2) + pow(p1.Y - p2.Y, 2));
+float distance(Point p1, Point p2) {
+    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
-
-class K_Means
-{
-public:
-    vector<Point> arr;
-    vector<Point> centroids;
-
-    int N, K;
-    int LL, UL;
-    K_Means(int n, int ll = 0, int ul = 100, int k = 2)
-    {
-        N = n;
-        LL = ll;
-        UL = ul;
-        K = k;
-    }
-
-    void assign_centroids()
-    {
-        for (int i = 0; i < N; i++)
-        {
-            double min = euc(arr[i], centroids[0]);
-            Point *cent = &centroids[0];
-            for (int j = 1; j < K; j++)
-            {
-                double new_d = euc(arr[i], centroids[j]);
-                if (new_d < min)
-                {
-                    min = new_d;
-                    cent = &centroids[j];
+void kMeansClustering(vector<Point> &points, int k) {
+    vector<Point> centroids(k);
+    for (int i = 0; i < k; i++)
+        centroids[i] = points[i];
+    bool changed;
+    do {
+        changed = false;
+        for (auto &p : points) {
+            float minDist = numeric_limits<float>::max();
+            int clusterIndex = -1;
+            for (int i = 0; i < k; i++) {
+                float dist = distance(p, centroids[i]);
+                if (dist < minDist) {
+                    minDist = dist;
+                    clusterIndex = i;
                 }
             }
-            arr[i].centroid = cent;
-        }
-    }
-
-    void reassign()
-    {
-        for (auto &centroid : centroids)
-        {
-            centroid.X = 0;
-            centroid.Y = 0;
-        }
-
-        vector<int> count(K, 0);
-
-        for (auto &point : arr)
-        {
-            for (int i = 0; i < K; i++)
-            {
-                if (&centroids[i] == point.centroid)
-                {
-                    centroids[i].X += point.X;
-                    centroids[i].Y += point.Y;
-                    count[i]++;
-                }
+            if (p.cluster != clusterIndex) {
+                p.cluster = clusterIndex;
+                changed = true;
             }
         }
-
-        for (int i = 0; i < K; i++)
-        {
-            if (count[i] > 0)
-            {
-                centroids[i].X /= count[i];
-                centroids[i].Y /= count[i];
+        vector<Point> newCentroids(k);
+        vector<int> count(k, 0);
+        for (auto &p : points) {
+            newCentroids[p.cluster].x += p.x;
+            newCentroids[p.cluster].y += p.y;
+            count[p.cluster]++;
+        }
+        for (int i = 0; i < k; i++) {
+            if (count[i] != 0) {
+                newCentroids[i].x /= count[i];
+                newCentroids[i].y /= count[i];
             }
         }
+        centroids = newCentroids;
+    } while (changed);
+}
+int main() {
+    int k = 2;
+    vector<Point> points = { {1.0, 1.0, -1}, {1.5, 2.0, -1}, {3.0, 4.0, -1},
+                             {5.0, 7.0, -1}, {3.5, 5.0, -1}, {4.5, 5.0, -1}, {3.5, 4.5, -1} };
+
+    kMeansClustering(points, k);
+    for (auto &p : points) {
+        cout << "Point (" << p.x << ", " << p.y << ") in Cluster " << p.cluster + 1 << endl;
     }
-
-    void random_cents()
-    {
-        uniform_int_distribution<int> dist(0, N - 1);
-        unordered_set<int> selected_indices;
-
-        while (selected_indices.size() < K)
-        {
-            int a = dist(gen);
-            if (selected_indices.insert(a).second)
-            {
-                centroids.push_back(arr[a]);
-            }
-        }
-    }
-
-    void generate_points()
-    {
-        for (int i = 0; i < N; i++)
-        {
-            Point p(LL, UL);
-            arr.emplace_back(p);
-        }
-    }
-
-    void display()
-    {
-        FILE *gnuplotPipe = _popen("gnuplot -persist", "w");
-
-        if (gnuplotPipe)
-        {
-            fprintf(gnuplotPipe, "set title 'K-Means Clustering'\n");
-            fprintf(gnuplotPipe, "set xlabel 'X-axis'\n");
-            fprintf(gnuplotPipe, "set ylabel 'Y-axis'\n");
-
-            const char *colors[] = {"red", "green", "blue", "orange", "purple", "cyan", "brown", "pink", "gray", "magenta", "violet"};
-
-            fprintf(gnuplotPipe, "plot ");
-
-            for (int i = 0; i < K; i++)
-            {
-                if (i > 0)
-                    fprintf(gnuplotPipe, ", ");
-                fprintf(gnuplotPipe, "'-' using 1:2 with points pointtype 7 pointsize 0.5 lc rgb '%s' title 'Cluster %d'", colors[i % 10], i);
-            }
-
-            fprintf(gnuplotPipe, ", '-' using 1:2 with points pointtype 7 pointsize 1.5 lc rgb 'black' title 'Centroids'\n");
-
-            for (int i = 0; i < K; i++)
-            {
-                for (auto &p : arr)
-                {
-                    if (p.centroid == &centroids[i])
-                    {
-                        fprintf(gnuplotPipe, "%f %f\n", p.X, p.Y);
-                    }
-                }
-                fprintf(gnuplotPipe, "e\n");
-            }
-
-            for (auto &c : centroids)
-            {
-                fprintf(gnuplotPipe, "%f %f\n", c.X, c.Y);
-            }
-            fprintf(gnuplotPipe, "e\n");
-
-            fflush(gnuplotPipe);
-            _pclose(gnuplotPipe);
-        }
-        else
-        {
-            cerr << "Could not open Gnuplot!" << endl;
-        }
-    }
-};
-
-int main()
-{
-
-    cout << "Enter number of centroids: " << endl;
-    int k1;
-    cin >> k1;
-    int total;
-    cout << "Enter total number of datapoints: " << endl;
-    cin >> total;
-    auto start = high_resolution_clock::now(); // Start time
-
-    K_Means k(total, 0, 100, k1);
-    k.generate_points();
-    k.random_cents();
-    k.assign_centroids();
-
-    for (int i = 0; i < 5; i++)
-    {
-        k.reassign();
-    }
-
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(stop - start);
-
-    cout << "Execution Time: " << duration.count() << " ms" << endl;
-
-    k.display();
-
     return 0;
 }
--- kmeansdpcpp.cpp
+++ kmeansdpcpp.cpp
@@ -1,143 +1,84 @@
+
 #include <CL/sycl.hpp>
 #include <iostream>
 #include <vector>
-#include <random>
-#include <unordered_set>
-#include <chrono>
-
-class Point {
-public:
-    double X, Y;
-    int cluster_id;
-
-    Point(double x = 0, double y = 0) : X(x), Y(y), cluster_id(-1) {}
+#include <cmath>
+#include <limits>
+using namespace sycl;
+using namespace std;
+struct Point {
+    float x, y;
+    int cluster;
 };
-
-// Euclidean Distance Function
-double euc(const Point& p1, const Point& p2) {
-    return sycl::sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));
+float distance(Point p1, Point p2) {
+    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
-
-class KMeans {
-public:
-    std::vector<Point> points;
-    std::vector<Point> centroids;
-    int N, K;
-
-    KMeans(int n, int k) : N(n), K(k) {}
-
-    void generate_points() {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_real_distribution<double> dist(0.0, 100.0);
-        
-        for (int i = 0; i < N; i++) {
-            points.emplace_back(dist(gen), dist(gen));
-        }
-    }
-
-    void initialize_centroids() {
-        std::unordered_set<int> selected_indices;
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> dist(0, N - 1);
-        
-        while (selected_indices.size() < K) {
-            int idx = dist(gen);
-            if (selected_indices.insert(idx).second) {
-                centroids.push_back(points[idx]);
-            }
-        }
-    }
-
-    void assign_clusters(sycl::queue& q) {
-        sycl::buffer<Point, 1> points_buf(points.data(), sycl::range<1>(N));
-        sycl::buffer<Point, 1> centroids_buf(centroids.data(), sycl::range<1>(K));
-        
-        q.submit([&](sycl::handler& h) {
-            auto points_acc = points_buf.get_access<sycl::access::mode::read_write>(h);
-            auto centroids_acc = centroids_buf.get_access<sycl::access::mode::read>(h);
-            int local_K = K;  // Local copy
-
-            h.parallel_for(sycl::range<1>(N), [=](sycl::id<1> i) {
-                double min_dist = euc(points_acc[i], centroids_acc[0]);
-                int closest = 0;
-                
-                for (int j = 1; j < local_K; j++) {
-                    double dist = euc(points_acc[i], centroids_acc[j]);
-                    if (dist < min_dist) {
-                        min_dist = dist;
-                        closest = j;
+void kMeansClustering(vector<Point> &points, int k) {
+    queue q;
+    buffer<Point, 1> points_buf(points.data(), range<1>(points.size()));
+    buffer<Point, 1> centroids_buf(k);
+    q.submit([&](handler &h) {
+        auto points_acc = points_buf.get_access<access::mode::read_write>(h);
+        auto centroids_acc = centroids_buf.get_access<access::mode::write>(h);
+        h.parallel_for(range<1>(k), [=](id<1> i) {
+            centroids_acc[i] = points_acc[i];
+        });
+    });
+    bool changed;
+    do {
+        changed = false;
+        buffer<bool, 1> changed_buf(&changed, range<1>(1));
+        q.submit([&](handler &h) {
+            auto points_acc = points_buf.get_access<access::mode::read_write>(h);
+            auto centroids_acc = centroids_buf.get_access<access::mode::read>(h);
+            auto changed_acc = changed_buf.get_access<access::mode::write>(h);
+            h.parallel_for(range<1>(points.size()), [=](id<1> i) {
+                float minDist = numeric_limits<float>::max();
+                int clusterIndex = -1;
+                for (int j = 0; j < k; j++) {
+                    float dist = distance(points_acc[i], centroids_acc[j]);
+                    if (dist < minDist) {
+                        minDist = dist;
+                        clusterIndex = j;
                     }
                 }
-                points_acc[i].cluster_id = closest;
+                if (points_acc[i].cluster != clusterIndex) {
+                    points_acc[i].cluster = clusterIndex;
+                    *changed_acc = true;
+                }
             });
-        }).wait();
-    }
+        });
+        vector<Point> newCentroids(k);
+        vector<int> count(k, 0);
+        q.submit([&](handler &h) {
+            auto points_acc = points_buf.get_access<access::mode::read>(h);
+            h.parallel_for(range<1>(points.size()), [=](id<1> i) {
+                atomic<float> xSum(0.0f), ySum(0.0f);
+                atomic<int> c(0);
+                xSum.fetch_add(points_acc[i].x);
+                ySum.fetch_add(points_acc[i].y);
+                c.fetch_add(1);
 
-    void update_centroids(sycl::queue& q) {
-        std::vector<Point> new_centroids(K, Point(0, 0));
-        std::vector<int> count(K, 0);
-        
-        sycl::buffer<Point, 1> points_buf(points.data(), sycl::range<1>(N));
-        sycl::buffer<Point, 1> centroids_buf(new_centroids.data(), sycl::range<1>(K));
-        sycl::buffer<int, 1> count_buf(count.data(), sycl::range<1>(K));
-
-        q.submit([&](sycl::handler& h) {
-            auto points_acc = points_buf.get_access<sycl::access::mode::read>(h);
-            auto centroids_acc = centroids_buf.get_access<sycl::access::mode::read_write>(h);
-            auto count_acc = count_buf.get_access<sycl::access::mode::read_write>(h);
-            
-            h.parallel_for(sycl::range<1>(N), [=](sycl::id<1> i) {
-                int cid = points_acc[i].cluster_id;
-                sycl::atomic_ref<double, sycl::memory_order::relaxed, sycl::memory_scope::device> 
-                    atomic_x(centroids_acc[cid].X), atomic_y(centroids_acc[cid].Y);
-                sycl::atomic_ref<int, sycl::memory_order::relaxed, sycl::memory_scope::device> 
-                    atomic_count(count_acc[cid]);
-                
-                atomic_x.fetch_add(points_acc[i].X);
-                atomic_y.fetch_add(points_acc[i].Y);
-                atomic_count.fetch_add(1);
+                newCentroids[points_acc[i].cluster].x = xSum.load() / c.load();
+                newCentroids[points_acc[i].cluster].y = ySum.load() / c.load();
             });
-        }).wait();
-
-        // Compute final centroid positions on the host
-        for (int i = 0; i < K; i++) {
-            if (count[i] > 0) {
-                new_centroids[i].X /= count[i];
-                new_centroids[i].Y /= count[i];
-            }
-        }
-
-        centroids = new_centroids;
-    }
-
-    void run(sycl::queue& q, int iterations) {
-        for (int i = 0; i < iterations; i++) {
-            assign_clusters(q);
-            update_centroids(q);
-        }
-    }
-
-    
-};
-
+        });
+        q.submit([&](handler &h) {
+            auto centroids_acc = centroids_buf.get_access<access::mode::write>(h);
+            h.parallel_for(range<1>(k), [=](id<1> i) {
+                centroids_acc[i] = newCentroids[i];
+            });
+        });
+    } while (changed);
+}
 int main() {
-    sycl::queue q(sycl::default_selector_v);
+    int k = 2;
+    vector<Point> points = { {1.0, 1.0, -1}, {1.5, 2.0, -1}, {3.0, 4.0, -1},
+                             {5.0, 7.0, -1}, {3.5, 5.0, -1}, {4.5, 5.0, -1}, {3.5, 4.5, -1} };
 
-    
-    
-    auto start = std::chrono::high_resolution_clock::now();
-    
-    KMeans kmeans(10000, 50);
-    kmeans.generate_points();
-    kmeans.initialize_centroids();
-    kmeans.run(q, 5);
-    
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-    std::cout << "Execution Time: " << duration.count() << " ms" << std::endl;
-    
-    
+    kMeansClustering(points, k);
+    for (auto &p : points) {
+        cout << "Point (" << p.x << ", " << p.y << ") in Cluster " << p.cluster + 1 << endl;
+    }
     return 0;
 }
